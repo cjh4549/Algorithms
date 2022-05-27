96a3a0a5d7e1872869f193a4b0efcac36e22992b
JunChoiQ4 <96628396+JunChoiQ4@users.noreply.github.com>
2022-05-27 15:23:02 -0400

    first commit

---
+++ isSubsequence.js
@@ -0,0 +1,11 @@
+//Test Case
+// {
+//     "array": [5, 1, 22, 25, 6, -1, 8, 10],
+//     "sequence": [1, 6, -1, 10]
+//   }
+
+function isValidSubsequence(array, sequence) {
+    // Write your code here.
+    
+
+  }
