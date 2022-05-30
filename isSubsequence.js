436f042be9b74e685b2327a3be9b60b919700971
JunChoiQ4 <96628396+JunChoiQ4@users.noreply.github.com>
2022-05-30 18:39:38 -0400

    initial solution

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,11 +1,24 @@
 //Test Case
-// {
-//     "array": [5, 1, 22, 25, 6, -1, 8, 10],
-//     "sequence": [1, 6, -1, 10]
-//   }
+testCase = {
+    "array": [5, 1, 22, 25, 6, -1, 8, 10],
+    "sequence": [1, 6, -1, 10]
+  }
 
 function isValidSubsequence(array, sequence) {
     // Write your code here.
+
+    let comparisonArr = [];
+
+    sequence.forEach(element => {
+      if(array.includes(element)) {
+        comparisonArr.push(element);
+      } else {
+        return //terminate the function
+      }
+    });
+
+    return JSON.stringify(comparisonArr) == JSON.stringify(sequence);
     
-    //changed
   }
+
+console.log(isValidSubsequence(testCase.array, testCase.sequence));
