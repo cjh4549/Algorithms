84b6a592e443080cc046800a351fd85522d40c35
cjh4549 <cjh4549@gmail.com>
2022-06-01 22:14:25 -0400

    first solution submitted

---
+++ binarySearch.js
@@ -0,0 +1,15 @@
+//You must write an algorithm with O(log n) runtime complexity.
+//O(log n) is like looking for a person in a phone book (ex. divide-and-conquer)
+
+//Solution 1
+// var search = function(nums, target) {
+//     return nums.indexOf(target)
+// };
+
+//Left and Right pivot approach
+var search = function(nums, target){
+    
+}
+
+search([-1,0,3,5,9,12], 9);
+
