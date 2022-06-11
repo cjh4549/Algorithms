c2f071a1f840e0a86088a1580d681ed11bc51015
Jun Choi <48137126+cjh4549@users.noreply.github.com>
2022-06-11 14:14:05 -0400

    Fixed some minor issue

--- flatten.js
+++ flatten.js
@@ -3,15 +3,17 @@
 const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10];
 
 function flatten(arr) {
-  arr.reduce((acc, item) => {
+  const newArray = arr.reduce((acc, item) => {
     if (Array.isArray(item)) {
       acc = acc.concat(flatten(item));
     } else {
       acc.push(item);
     }
-    
-    return acc;
+
+    return acc; //Doesn't return acc by default
   }, []);  
+  
+  return newArray; //The actual flattened accumulated array
 }
 
 flatten(exampleArray); // [1,2,3,4,5,6,7,8,9,10]
