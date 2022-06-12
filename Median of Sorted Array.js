ce8c05ede924d268c7aa5aa31af01fb2d7602be1
Jun Choi <48137126+cjh4549@users.noreply.github.com>
2022-06-12 15:14:03 -0400

    correctly sort in numerical order

--- Median of Sorted Array.js
+++ Median of Sorted Array.js
@@ -1,20 +1,22 @@
 var findMedianSortedArrays = function(nums1, nums2) {
     const newNum = nums1.concat(nums2); 
-    newNum.sort();
+    newNum.sort(function(a, b){
+        return a - b;
+    });
     
     const startIndex = 0;
     const endIndex = newNum.length - 1;
     
-    if(newNum.length % 2 != 0 ) {
+    if(newNum.length % 2 !== 0 ) {
         const oddMedian = newNum[Math.floor(endIndex / 2)];
         console.log(`merged array = ${newNum} and median is ${oddMedian}`);
         return oddMedian;
     } 
-    else if (newNum.length % 2 == 0) {
+    else if (newNum.length % 2 === 0) {
         const evenMedian = (newNum[Math.floor(endIndex / 2)] + newNum[Math.ceil(endIndex / 2)]) / 2;
         console.log(`merged array = ${newNum} and median is ${evenMedian} `);
         return evenMedian;
     }
 };
 
-findMedianSortedArrays([1,2], [3,4]); 
+findMedianSortedArrays([-1, -2], [3,4]); 
